f3a523ab4b85447043994935e9b6f62abdbfcfea
emiltx <crocomile@gmail.Com>
2024-05-12 18:03:11 +0200

    correction locality

--- src/modelPackage/Locality.java
+++ src/modelPackage/Locality.java
@@ -6,22 +6,31 @@ public class Locality {
     private Integer zipCode;
     private Country country;
 
-    public Locality(String city, Integer zipCode, Country country){
+    public Locality(Integer id, String city, Integer zipCode, Country country){
+        setId(id);
         setCity(city);
         setZipCode(zipCode);
         setCountry(country);
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public void setCity(String city) {
         this.city = city;
     }
 
-    public void setZipCode(Integer zip_code) {
+    public void setZipCode(Integer zipCode) {
         this.zipCode = zipCode;
     }
 
     public void setCountry(Country country) {
         this.country = country;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getCity() {
