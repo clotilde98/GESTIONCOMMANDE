733d5b9eb92baa3a68d9b018b2591bd85661d353
emiltx <crocomile@gmail.Com>
2024-05-12 18:58:38 +0200

    add CustomerDataAccess

---
+++ src/dataAccessPackage/CustomerDataAccess.java
@@ -0,0 +1,20 @@
+package dataAccessPackage;
+
+import modelPackage.Customer;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CustomerDataAccess {
+    void addCustomer(Customer customer) throws SQLException;
+
+    Customer getCustomer(Integer customerNumber);
+
+    ArrayList<Customer> getAllCustomers();
+
+    Customer updateCustomer();
+
+    void deleteCustomer(Customer customer);
+
+    Customer getUser(String email, String password) throws SQLException;
+}
