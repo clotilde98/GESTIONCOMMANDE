604efc5f01b5f2f6262a869df72ec428ab1de2f5
emiltx <crocomile@gmail.Com>
2024-05-15 12:59:36 +0200

    add temporary main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-import viewPackage.Menu;
+
+import viewPackage.*;
+
 
 import javax.swing.*;
+import java.time.LocalDate;
 
 
-public class Main extends JFrame{
+public class
+Main extends JFrame{
 
 
-    public static void main (String []args){
+    public static void main (String []args) {
 
-        Menu menu = new Menu();
-
+        Login login = new Login();
+        //Menu menu = new Menu();
+        //Invoice
+        //TempSearchOneFrame temp = new TempSearchOneFrame(3);
+        //Product History
+        //TempSearchTwo temp = new TempSearchTwo("Belle");
+        //Product Info
+        //TempSearchThree temp = new TempSearchThree((double)150);
+        //Task
+        //TempTask temp = new TempTask(3,LocalDate.parse("2023-01-01"));
 
     }
 
