e61bbe16c61a345c69a3a5ddc22199162cca1c1e
emiltx <crocomile@gmail.Com>
2024-05-25 17:52:23 +0200

    small fix

--- src/viewPackage/CustomerTableModel.java
+++ src/viewPackage/CustomerTableModel.java
@@ -15,6 +15,7 @@ public class CustomerTableModel extends AbstractTableModel {
 
     public CustomerTableModel(ArrayList<Customer> customers)
     { columnNames = new ArrayList<>();
+        columnNames.add("Numéro Client");
         columnNames.add("Prénom");
         columnNames.add("Nom");
         columnNames.add("Email");
@@ -57,30 +58,32 @@ public class CustomerTableModel extends AbstractTableModel {
         switch (columnIndex) {
 
             case 0:
-                return customer.getFirstName();
+                return customer.getNumber();
             case 1:
-                return customer.getLastName();
+                return customer.getFirstName();
             case 2:
-                return customer.getEmail();
+                return customer.getLastName();
             case 3:
-                return customer.getPhoneNumber();
+                return customer.getEmail();
             case 4:
-                return customer.getGender();
+                return customer.getPhoneNumber();
             case 5:
-                return customer.getPassword();
+                return customer.getGender();
             case 6:
-                return customer.getBirthday();
+                return customer.getPassword();
             case 7:
-                return customer.getIsAdmin();
+                return customer.getBirthday();
             case 8:
-                return customer.getIsAdherent();
+                return customer.getIsAdmin();
             case 9:
-                return customer.getLocality().getCity();
+                return customer.getIsAdherent();
             case 10:
-                return customer.getStreet();
+                return customer.getLocality().getCity();
             case 11:
-                return customer.getStreetNumber();
+                return customer.getStreet();
             case 12:
+                return customer.getStreetNumber();
+            case 13:
                 return customer.getNumberSponsorised();
             default:
                 return null;
@@ -89,11 +92,11 @@ public class CustomerTableModel extends AbstractTableModel {
 
     public Class<?> getColumnClass(int columnIndex) {
         switch (columnIndex) {
-            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 9:case 10:
+            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 10:case 11:
                 return String.class;
-            case 7: case 8:
+            case 8: case 9:
                 return Boolean.class;
-            case 11: case 12:
+            case 0: case 12: case 13:
                 return Integer.class;
             default:
                 return Object.class;
